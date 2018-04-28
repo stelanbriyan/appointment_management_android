bf6338e1dd2c50ec8afa38766c9415a2b163ef71
stelansp <stelan@sp-solutions.biz>
2018-04-28 15:21:54 +0530

    appointment creater design fragment done

--- app/src/main/java/com/iit/appointmentmanagement/CreateAppointmentActivity.java
+++ app/src/main/java/com/iit/appointmentmanagement/CreateAppointmentActivity.java
@@ -3,13 +3,11 @@ package com.iit.appointmentmanagement;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+public class CreateAppointmentActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-
+        setContentView(R.layout.activity_create_appointment);
     }
 }
--- app/src/main/java/com/iit/appointmentmanagement/HomeFragment.java
+++ app/src/main/java/com/iit/appointmentmanagement/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.iit.appointmentmanagement;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
 
@@ -16,6 +18,14 @@ public class HomeFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_home,
                 container, false);
 
+        Button createAppointmentBtn = rootView.findViewById(R.id.createAppointmentBtn);
+        createAppointmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), CreateAppointmentActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
 
         return rootView;
     }
