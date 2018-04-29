31d2094235bb13c3a51faa7362b58004a59e4667
stelansp <stelan@sp-solutions.biz>
2018-04-29 12:52:41 +0530

    search filter method added

--- app/src/main/java/com/iit/appointmentmanagement/SearchAppointmentActivity.java
+++ app/src/main/java/com/iit/appointmentmanagement/SearchAppointmentActivity.java
@@ -13,14 +13,19 @@ import com.iit.appointmentmanagement.adapter.CustomListAdapter;
 import com.iit.appointmentmanagement.database_sqlite.DBHandler;
 import com.iit.appointmentmanagement.entity.Appointment;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author Stelan Briyan
  */
 public class SearchAppointmentActivity extends AppCompatActivity {
+    private DateFormat shortDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     private DBHandler dbHandler;
+    private ListView listView;
 
     private List<Appointment> appointments;
 
@@ -35,16 +40,58 @@ public class SearchAppointmentActivity extends AppCompatActivity {
 
         final EditText searchText = findViewById(R.id.searchTextValue);
 
-        final ListView listView = findViewById(R.id.searchResultList);
+        listView = findViewById(R.id.searchResultList);
         listView.setAdapter(new CustomListAdapter(appointments, getLayoutInflater()));
 
         Button searchButton = findViewById(R.id.searchABtn);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                appointments = dbHandler.findAppointments(searchText.getText().toString());
-                listView.setAdapter(new CustomListAdapter(appointments, getLayoutInflater()));
+
+                /**
+                 * Find all appointments.
+                 */
+                appointments = dbHandler.findAppointments();
+                filter(searchText.getText().toString());
+
             }
         });
+    }
+
+    /**
+     * String text filter.
+     *
+     * @param searchText String
+     */
+    public void filter(String searchText) {
+        List<Appointment> filterAppointments = new ArrayList<>();
+
+        if (this.appointments != null && searchText != null) {
+            for (Appointment appointment : this.appointments) {
+
+
+                /**
+                 * Check the matching
+                 */
+                if (appointment.getTitle() != null && appointment.getTitle().contains(searchText)) {
+                    filterAppointments.add(appointment);
+                } else if (appointment.getDetail() != null && appointment.getDetail().contains(searchText)) {
+                    filterAppointments.add(appointment);
+                } else if (appointment.getAppointmentDate() != null) {
+                    String format = shortDateFormat.format(appointment.getAppointmentDate());
+                    if (format.contains(searchText)) {
+                        filterAppointments.add(appointment);
+                    }
+                }
+
+            }
+        }
+
+        this.appointments = filterAppointments;
+
+        /**
+         * Update the list view.
+         */
+        listView.setAdapter(new CustomListAdapter(appointments, getLayoutInflater()));
     }
 }
